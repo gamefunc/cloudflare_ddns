57c090e9dacc1d33c00018220ef2885fb3163842
gamefunc <32686647@qq.com>
2019-04-22 05:32:31 +0800

    Update cloudflare_ddns.py

--- cloudflare_ddns.py
+++ cloudflare_ddns.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
 
     for parameter in paramets.keys():
         if inputParamets.count(parameter) > 1:
-            raise TypeError(f"{parameter} 大于1个")
+            raise TypeError(f"{parameter} input > 1")
 
     for parameter in paramets.keys():
         try:
